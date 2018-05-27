b4ca1ab274de0b0598052304fb590bc3548f98d5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-import fresh from "@unction/fresh"
-import mergeRight from "@unction/mergeright"
-import {either} from "ramda"
-import recordFrom from "@unction/recordfrom"
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import isObject from "@unction/isobject"
-import isArray from "@unction/isarray"
-
-const isEitherObjectOrArray = either(isObject)(isArray)
-
-export default function withoutKeyRecursive (key: KeyType): Function {
-  return function withoutKeyRecursiveKey (original: FunctorType): FunctorType {
-    return reduceWithValueKey(
-      function withoutKeyRecursiveKeyIterable (accumulated: FunctorType): Function {
-        const accumulatedMerge = mergeRight(accumulated)
-
-        return function withoutKeyRecursiveKeyIterableValue (current: any): Function {
-          const isIterable = isEitherObjectOrArray(current)
-
-          return function withoutKeyRecursiveKeyIterableValueKey (index: KeyType): FunctorType {
-            if (key === index) {
-              return accumulated
-            }
-
-            if (isIterable) {
-              return accumulatedMerge(recordFrom([index])(withoutKeyRecursive(key)(current)))
-            }
-
-            return accumulatedMerge(recordFrom([index])(current))
+import fresh from "@unction/fresh";
+import mergeRight from "@unction/mergeright";
+import { either } from "ramda";
+import recordFrom from "@unction/recordfrom";
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import isObject from "@unction/isobject";
+import isArray from "@unction/isarray";
+const isEitherObjectOrArray = either(isObject)(isArray);
+export default function withoutKeyRecursive(key) {
+  return function withoutKeyRecursiveKey(original) {
+    return reduceWithValueKey(function withoutKeyRecursiveKeyIterable(accumulated) {
+      const accumulatedMerge = mergeRight(accumulated);
+      return function withoutKeyRecursiveKeyIterableValue(current) {
+        const isIterable = isEitherObjectOrArray(current);
+        return function withoutKeyRecursiveKeyIterableValueKey(index) {
+          if (key === index) {
+            return accumulated;
           }
-        }
-      }
-    )(
-      fresh(original)
-    )(
-      original
-    )
-  }
+
+          if (isIterable) {
+            return accumulatedMerge(recordFrom([index])(withoutKeyRecursive(key)(current)));
+          }
+
+          return accumulatedMerge(recordFrom([index])(current));
+        };
+      };
+    })(fresh(original))(original);
+  };
 }
