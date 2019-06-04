889763eb5e45099171846b6962572072a35ca650
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-06-03 23:10:15 -0700

    Switching away from deprecated functions

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import fresh from "@unction/fresh";
 import mergeRight from "@unction/mergeright";
-import {either} from "ramda";
-import recordFrom from "@unction/recordfrom";
+import objectFrom from "@unction/objectfrom";
 import reduceWithValueKey from "@unction/reducewithvaluekey";
 import isObject from "@unction/isobject";
 import isArray from "@unction/isarray";
-const isEitherObjectOrArray = either(isObject)(isArray);
+
+const isEitherObjectOrArray = (value) => isObject(value) || isArray(value);
 
 export default function withoutKeyRecursive (key) {
   return function withoutKeyRecursiveKey (original) {
     return reduceWithValueKey((accumulated) => {
       const accumulatedMerge = mergeRight(accumulated);
 
-
       return function withoutKeyRecursiveKeyIterableValue (current) {
         const isIterable = isEitherObjectOrArray(current);
-
 
         return function withoutKeyRecursiveKeyIterableValueKey (index) {
           if (key === index) {
@@ -23,10 +21,10 @@ export default function withoutKeyRecursive (key) {
           }
 
           if (isIterable) {
-            return accumulatedMerge(recordFrom([index])(withoutKeyRecursive(key)(current)));
+            return accumulatedMerge(objectFrom([index])(withoutKeyRecursive(key)(current)));
           }
 
-          return accumulatedMerge(recordFrom([index])(current));
+          return accumulatedMerge(objectFrom([index])(current));
         };
       };
     })(fresh(original))(original);
